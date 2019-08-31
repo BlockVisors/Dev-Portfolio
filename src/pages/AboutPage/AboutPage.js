960a1a7bd49eba3b61060b7f94a17c931e9e5336
Dameon Green <dameongreen@gmail.com>
2019-08-31 17:14:40 -0400

    initial commit

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ScrollToNext from '@components/ScrollToNext';
+import './style.scss';
+
+const AboutPage = (props, context) => {
+  const {
+    theme: { colorPrimary, colorHighlight, bgPrimary, textPrimary }
+  } = context;
+
+  return (
+    <div className="about-page" style={{ backgroundColor: bgPrimary }}>
+      <style jsx="true">
+        {`
+          .highlight {
+            background-color: ${colorHighlight};
+          }
+          ::selection {
+            background-color: ${colorHighlight};
+          }
+        `}
+      </style>
+      <div className="content-grid">
+        <h1 style={{ color: colorPrimary }}>About</h1>
+        <div className="about-wrapper">
+          <div className="about-content" style={{ color: textPrimary }}>
+            <p>
+              I like <span className="highlight">all things Blockchain</span> and
+              i am focused on building the future.
+            </p>
+            <p>
+              My goal is the build useful applications that solve{' '}
+              <span className="highlight"> real Problems</span>. I think that working with open-source 
+              communities within this space is important.. I also like to{' '}
+              <span className="highlight">teach</span> what I learn to others. Thus, we have built
+              Blockvisors Education to make knowledge available to many more people. 
+            </p>
+            <p>
+              I built this site <span className="highlight">from scratch</span>.
+              By scratch, I mean <i>absolutely from scratch</i>{' '}
+              <span className="highlight">
+                without any UI library/framework
+              </span>{' '}
+              (except React though) and had so much fun along the way.
+            </p>
+            <p>
+              React-Redux, Node.js, Ruby on Rails, Javascript, Spring Boot, Python, Swift(IOS), Docker and
+              AWS are the main tricks up my sleeve. I like to explore new frameworks and libraries when
+              I have time. 
+            </p>
+            <p className="text-emoji" style={{ color: colorPrimary }}>
+              \ (•◡•) /
+            </p>
+          </div>
+        </div>
+      </div>
+      <ScrollToNext pageSelector=".portfolio-page" />
+    </div>
+  );
+};
+
+AboutPage.contextTypes = {
+  theme: PropTypes.any
+};
+
+export default AboutPage;
